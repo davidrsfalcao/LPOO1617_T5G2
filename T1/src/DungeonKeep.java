57014a85a9d66365d517226d00e2f20c5b97f136
davidrsfalcao <davidrsfalcao@gmail.com>
2017-02-18 23:17:01 +0000

    Ogre's movement working

--- T1/src/DungeonKeep.java
+++ T1/src/DungeonKeep.java
@@ -27,13 +27,145 @@ public class DungeonKeep {
 	
 	Hero heroi;
 	Guard guarda;
+	Ogre ogre;
 
+	//funções gerais
 	public void clearScreen() {
 		for (int i = 0; i < 100; ++i)
 			System.out.println();
 
 	}
 
+	void draw(char[][] matriz) {
+		clearScreen();
+		for (char[] mat : matriz) {
+			for (char mat1 : mat) {
+				System.out.print(mat1);
+			}
+			System.out.println();
+
+		}
+	}
+
+	
+	// funções nível 2
+	public void movementO()
+	{
+		ogre.movement();
+		
+		if (matriz1[ogre.cordY_temp][ogre.cordX_temp] == ' ') {
+			matriz1[ogre.cordY][ogre.cordX] = ' '; // atualiza o tabuleiro
+			matriz1[ogre.cordY_temp][ogre.cordX_temp] = 'O'; // atualiza o tabuleiro
+														
+			ogre.cordX = ogre.cordX_temp; // atualiza a cord X
+			ogre.cordY = ogre.cordY_temp; // atualiza a cord Y
+		}
+		
+		else if (matriz1[ogre.cordY_temp][ogre.cordX_temp] == 'X') {
+			ogre.cordX_temp = ogre.cordX; // não há movimento
+			ogre.cordY_temp = ogre.cordY; // X,Y-temp invalidos
+		}
+		
+	}
+	
+	public boolean movementH_lvl2()
+	{
+		heroi.movement(); // movimento temp (condições não testadas)
+
+		// verficar se a posição temp se encontra dentro do tabuleiro
+		// em princípio seria desnecessária
+
+		if ((heroi.cordX_temp >= 0) && (heroi.cordX_temp < 9) && (heroi.cordY_temp >= 0) && (heroi.cordY_temp < 9)) {
+
+			if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == ' ') {
+				matriz1[heroi.cordY][heroi.cordX] = ' '; // atualiza o tabuleiro
+				
+				if (heroi.have_key) {
+					matriz1[heroi.cordY_temp][heroi.cordX_temp] = 'K';
+				}
+				else {
+					matriz1[heroi.cordY_temp][heroi.cordX_temp] = 'H';
+				}
+				
+				
+				heroi.cordX = heroi.cordX_temp; // atualiza a cord X
+				heroi.cordY = heroi.cordY_temp; // atualiza a cord Y
+			}
+
+			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'X') {
+				heroi.cordX_temp = heroi.cordX; // não há movimento
+				heroi.cordY_temp = heroi.cordY; // X,Y-temp invalidos
+			}
+
+			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'I') {
+				heroi.cordX_temp = heroi.cordX; // não há movimento
+				heroi.cordY_temp = heroi.cordY; // X,Y-temp invalidos
+			}
+
+			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'k') {
+				matriz1[heroi.cordY_temp][heroi.cordX_temp] = ' ';
+				matriz1[heroi.cordY][heroi.cordX] = ' '; // atualiza o tabuleiro
+				matriz1[heroi.cordY_temp][heroi.cordX_temp] = 'K'; 
+				heroi.have_key = true;
+				heroi.cordX = heroi.cordX_temp; // atualiza a cord X
+				heroi.cordY = heroi.cordY_temp; // atualiza a cord Y
+			}
+
+			else if (heroi.cordX_temp == 0)
+				return true;
+
+		}
+
+		return false;
+	}
+	
+	public int level2() {
+		
+		heroi.cordX = 1;
+		heroi.cordY = 7;
+		heroi.cordX_temp = 1;
+		heroi.cordY_temp = 7;
+		
+		boolean fim = false;
+
+		while (!fim) {
+
+			draw(matriz1);
+
+			if (movementH_lvl2()) {
+				return 0;
+			}
+			movementO();
+
+
+		}
+
+		return 1;
+	}
+
+	
+	// funções nível 1
+	public int colisionHG() {
+		int capture = 0; // 1 for yes and 0 for no
+		// zona de captura do guarda
+
+		int[][] catch_zone = { { guarda.cordX - 1, guarda.cordY - 1 }, { guarda.cordX - 1, guarda.cordY },
+				{ guarda.cordX - 1, guarda.cordY + 1 }, { guarda.cordX, guarda.cordY - 1 },
+				{ guarda.cordX + 1, guarda.cordY - 1 }, { guarda.cordX + 1, guarda.cordY },
+				{ guarda.cordX + 1, guarda.cordY + 1 }, { guarda.cordX, guarda.cordY + 1 },
+				{ guarda.cordX, guarda.cordY }, };
+
+		for (int[] cords : catch_zone) {
+			if ((cords[0] == heroi.cordX) && (cords[1] == heroi.cordY)) {
+				capture = 1;
+				break;
+			}
+
+		}
+
+		return capture;
+	}
+	
 	public void movementG() {
 		matriz[guarda.cordY][guarda.cordX] = ' ';
 
@@ -102,119 +234,7 @@ public class DungeonKeep {
 		return false;
 
 	}
-
-	public boolean movementH_lvl2()
-	{
-		heroi.movement(); // movimento temp (condições não testadas)
-
-		// verficar se a posição temp se encontra dentro do tabuleiro
-		// em princípio seria desnecessária
-
-		if ((heroi.cordX_temp >= 0) && (heroi.cordX_temp < 9) && (heroi.cordY_temp >= 0) && (heroi.cordY_temp < 9)) {
-
-			if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == ' ') {
-				matriz1[heroi.cordY][heroi.cordX] = ' '; // atualiza o tabuleiro
-				matriz1[heroi.cordY_temp][heroi.cordX_temp] = 'H'; // atualiza o
-																	// tabuleiro
-				heroi.cordX = heroi.cordX_temp; // atualiza a cord X
-				heroi.cordY = heroi.cordY_temp; // atualiza a cord Y
-			}
-
-			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'X') {
-				heroi.cordX_temp = heroi.cordX; // não há movimento
-				heroi.cordY_temp = heroi.cordY; // X,Y-temp invalidos
-			}
-
-			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'I') {
-				heroi.cordX_temp = heroi.cordX; // não há movimento
-				heroi.cordY_temp = heroi.cordY; // X,Y-temp invalidos
-			}
-
-			else if (matriz1[heroi.cordY_temp][heroi.cordX_temp] == 'k') {
-				matriz1[heroi.cordY_temp][heroi.cordX_temp] = ' ';
-				for (int k = 0; k <= 9; k++) {
-					for (int h = 0; h <= 9; h++) {
-						if (matriz[k][h] == 'I') {
-							matriz[k][h] = 'S';
-						}
-					}
-				}
-				matriz1[heroi.cordY][heroi.cordX] = ' '; // atualiza o tabuleiro
-				matriz1[heroi.cordY_temp][heroi.cordX_temp] = 'H'; // atualiza o
-																	// tabuleiro
-				heroi.cordX = heroi.cordX_temp; // atualiza a cord X
-				heroi.cordY = heroi.cordY_temp; // atualiza a cord Y
-			}
-
-			else if (heroi.cordX_temp == 0)
-				return true;
-
-		}
-
-		return false;
-	}
 	
-	public int colisionHG() {
-		int capture = 0; // 1 for yes and 0 for no
-		// zona de captura do guarda
-
-		int[][] catch_zone = { { guarda.cordX - 1, guarda.cordY - 1 }, { guarda.cordX - 1, guarda.cordY },
-				{ guarda.cordX - 1, guarda.cordY + 1 }, { guarda.cordX, guarda.cordY - 1 },
-				{ guarda.cordX + 1, guarda.cordY - 1 }, { guarda.cordX + 1, guarda.cordY },
-				{ guarda.cordX + 1, guarda.cordY + 1 }, { guarda.cordX, guarda.cordY + 1 },
-				{ guarda.cordX, guarda.cordY }, };
-
-		for (int[] cords : catch_zone) {
-			if ((cords[0] == heroi.cordX) && (cords[1] == heroi.cordY)) {
-				capture = 1;
-				break;
-			}
-
-		}
-
-		return capture;
-	}
-
-	void draw(char[][] matriz) {
-		clearScreen();
-		for (char[] mat : matriz) {
-			for (char mat1 : mat) {
-				System.out.print(mat1);
-			}
-			System.out.println();
-
-		}
-	}
-
-	public int level2() {
-		
-		heroi.cordX = 1;
-		heroi.cordY = 7;
-		heroi.cordX_temp = 1;
-		heroi.cordY_temp = 7;
-		
-		boolean fim = false;
-
-		while (!fim) {
-
-			draw(matriz1);
-			
-			System.out.println("\n\nx" + heroi.cordX);
-			System.out.println("\n\nx1" + heroi.cordX_temp);
-			System.out.println("\n\ny" + heroi.cordY);
-			System.out.println("\n\ny1" + heroi.cordY_temp);
-
-			if (movementH_lvl2()) {
-				return 0;
-				
-			}
-
-
-		}
-
-		return 1;
-	}
-
 	public int level1() {
 
 		boolean fim = false;
@@ -238,12 +258,16 @@ public class DungeonKeep {
 
 	}
 
+	
+	// função inicial
 	public static void main(String[] args) {
 		DungeonKeep game = new DungeonKeep();
 		Hero H = new Hero(); // inicialização
 		game.heroi = H; // do heroi
 		Guard G = new Guard(); // inicialização
 		game.guarda = G; // do guarda
+		Ogre ogre = new Ogre();
+		game.ogre = ogre;
 
 		if (game.level1() == 0) {
 			if (game.level2() == 0) {
--- T1/src/Ogre.java
+++ T1/src/Ogre.java
@@ -1,13 +1,48 @@
+import java.util.Random;
+
 public class Ogre {
 	
 	public int cordX;
 	public int cordY;
 	public int cordX_temp;
 	public int cordY_temp;
+	boolean have_key;
 	
 	public void movement(){
 		
+		Random gerador = new Random();
+		int numero = gerador.nextInt(4);
+		
+		System.out.println(numero);
+		
+		switch(numero)
+		{
+		case 0:
+			cordX_temp--;  // move left
+			break;
+			
+		case 1:
+			cordX_temp++; // move right
+			break;
+			
+		case 2:
+			cordY_temp--; // move down
+			break;
+			
+		case 3:
+			cordY_temp++; // move up
+			
+		}
 		
 	}
 
+	public Ogre()
+	{
+		cordX = 4;
+		cordY = 1;
+		cordX_temp = 4;
+		cordY_temp = 1;
+		have_key = false;
+		
+	}
 }

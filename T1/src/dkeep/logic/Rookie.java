f15a9fbedbe2faac904dd83a0ba74d1efce38d81
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-02 21:15:37 +0000

    movimento guardas completo

--- T1/src/dkeep/logic/Rookie.java
+++ T1/src/dkeep/logic/Rookie.java
@@ -4,6 +4,7 @@ public class Rookie extends Guard {
 	
 	public Position moveCharacter(int MAP_SIZE)
 	{
+		increaseIndex();
 		position.changeTo(patch[index][0] , patch[index][1]);
 		
 		return position;
--- T1/src/dkeep/logic/Suspicious.java
+++ T1/src/dkeep/logic/Suspicious.java
@@ -1,10 +1,50 @@
 package dkeep.logic;
+import java.util.Random;
 
 public class Suspicious extends Guard {
 
+	private boolean front = true;
+	private int steps_front;
+	private int steps_back;
+	private Random rand = new Random();
+	
+	
+	public Suspicious()
+	{
+		super();
+		steps_front = rand.nextInt(5) + 3;
+		steps_back = rand.nextInt(3) + 1;
+		
+	}
 	
 	public Position moveCharacter(int MAP_SIZE)
 	{
+		if (front) {
+			increaseIndex();
+			position.changeTo(patch[index][0], patch[index][1]);
+			steps_front--;
+			
+		} else {
+			decreaseIndex();
+			position.changeTo(patch[index][0], patch[index][1]);
+			steps_back--;
+		}
+		
+		if (steps_front == 0)
+		{
+			steps_front = rand.nextInt(5) + 3;
+			front = false;
+		}
+		
+		if (steps_back == 0)
+		{
+			steps_back = rand.nextInt(3) + 1;
+			front = true;
+			
+		}
+		
+		
+		
 		return position;
 	}
 	

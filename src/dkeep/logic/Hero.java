df40eb8978de6f30d20290457b335439807f01c2
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-05 14:33:10 +0000

    Organizei e comentei a classe Hero

--- src/dkeep/logic/Hero.java
+++ src/dkeep/logic/Hero.java
@@ -1,32 +1,11 @@
 package dkeep.logic;
 
-import java.util.ArrayList;
-
 public class Hero extends Character{
 	
 	private Position last_position;
 	private boolean has_key;
 	private boolean is_armed;
 	
-	public Hero(int level){
-		
-		playing = false;
-		
-		if (level == 0)
-		{
-			position = new Position(1, 1, 'H');
-			is_armed = false;
-		}
-		else if (level == 1)
-		{
-			position = new Position(1, 7, 'A');
-			is_armed = true;
-		}
-		
-		has_key = false;
-		last_position = new Position(position.getX(), position.getY(), position.getRepresentation());
-	
-	}
 	
 	public Hero(int posX, int posY, boolean has_key, boolean is_armed)
 	{
@@ -45,10 +24,21 @@ public class Hero extends Character{
 		last_position = new Position(posX, posY, position.getRepresentation());
 	}
 	
+	/**
+	 * Implementation of abstract method. Unused by the hero
+	 * 
+	 */
 	public Position moveCharacter(int MAP_SIZE){
 		return null;
 	}
 
+	/**
+	 * Moves the Hero
+	 * 
+	 * @param MAP_SIZE
+	 * @param dir
+	 * @return temporary position
+	 */
 	public Position moveCharacter(int MAP_SIZE,int dir){
 		Position temp =  new Position(position.getX(), position.getY(), position.getRepresentation());
 		
@@ -77,30 +67,53 @@ public class Hero extends Character{
 		
 		return temp;
 	}	
-		
+	
+	/**
+	 * The hero come back to the last position
+	 * 
+	 */
+	public void comeBack()
+	{
+		position.copy(last_position);
+	}
+	
+	/**
+	 * Update the last position
+	 * 
+	 */
+	public void updateLastPosition()
+	{
+		last_position.copy(position);
+	}
+	
+	/**
+	 * Return if the hero has a key or not
+	 * 
+	 * @return true or false
+	 */
 	public boolean hasKey()
 	{
 		return has_key;
 	}
 	
+	/**
+	 * Hero catch a key
+	 * 
+	 */
 	public void pickUpKey()
 	{
 		has_key = true;
 		position.setRepresentation('K');
 	}
 	
+	/**
+	 * Return if the hero is armed or not
+	 * 
+	 * @return true or false
+	 */
 	public boolean is_armed()
 	{
 		return is_armed;
 	}
 	
-	public void updateLastPosition()
-	{
-		last_position.copy(position);
-	}
-	
-	public void comeBack()
-	{
-		position.copy(last_position);
-	}
 }

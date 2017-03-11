2ed1320e51fa97a717d10cf9f96d67a30578e26a
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-11 15:40:06 +0000

    Create Gui launcher

---
+++ src/dkeep/gui/Launcher.java
@@ -0,0 +1,14 @@
+package dkeep.gui;
+
+import java.io.IOException;
+
+
+public class Launcher{
+	
+	public static void main(String[] args) throws IOException {
+	
+		GameFrame window = new GameFrame();
+		//window.init();
+	}
+
+}

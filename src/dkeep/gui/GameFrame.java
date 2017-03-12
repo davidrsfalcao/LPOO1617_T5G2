12c322204ccbcd732e98526ae2061f10cc83cf7d
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-12 23:44:46 +0000

    update

--- src/dkeep/gui/GameFrame.java
+++ src/dkeep/gui/GameFrame.java
@@ -1,112 +1,95 @@
 package dkeep.gui;
 
+import java.awt.EventQueue;
 
-import dkeep.logic.GameConfig;
-
-import java.io.IOException;
-
-import javax.swing.JDialog;
 import javax.swing.JFrame;
+
+import dkeep.logic.*;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import javax.swing.JSlider;
+import java.awt.Component;
+import java.awt.Rectangle;
 
-public class GameFrame extends JFrame {
+public class GameFrame {
 
-	private JButton btnNewGame;
-	private JButton btnOptions;
-	private JButton btnQuitGame;
-	private JPanel buttonsPanel;
-	private JPanel buttonsPanel2;
-	//private GamePanel gamePanel;
-	private GameConfig gameConfig;
-	private JDialog options;
+	private JFrame frmMaze;
+	private Logic g;
+	private JTextArea printMaze;
+	private JLabel lblcurretnState;
+	private JLabel mazeSize;
+	private JButton btnUp;
+	private JButton btnDown;
+	private JButton btnLeft;
+	private JButton btnRight;
+	private JButton btnExit;
+	private JButton newMaze;
+	private JButton btnExitToMenu;
+	private GamePanel graphicsPanel;
+	private JSlider mazeDimensions;
+	private JSlider drakeNumber;
+	private JComboBox<String> gameMode;
+	private JButton btnRandomMaze;
+	private JButton btnNext;
 
-	public GameFrame() throws IOException {
-		setTitle("Dungeon Explorer");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-		//gamePanel = new GamePanel();
-		buttonsPanel = new JPanel();
-		buttonsPanel2 = new JPanel();
-		gameConfig = new GameConfig();
-		//options = new OptionsDialog(this, gamePanel, gameConfig);
-		//saveLoad = new SaveLoadDialog(this, gamePanel);
-
-		setUpButtons();
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		addButtons();
-		//getContentPane().add(gamePanel);
-		
-	}
-	
-	 
-	private void setUpButtons() {
-		// Play Game button
-		btnNewGame = new JButton("New Game");
-		btnNewGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) { {
-					setSize(642, 598);
-
-					Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-					setLocation(dim.width / 2 - getSize().width / 2, dim.height
-							/ 2 - getSize().height / 2);
-
-					// starting new game with new options
-					//gamePanel.startNewGame(gameConfig);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameFrame window = new GameFrame();
+					window.frmMaze.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		});
+	}
 
+	/**
+	 * Create the application.
+	 */
+	public GameFrame() {
+		initialize();
+	}
 
-		// Options button
-		btnOptions = new JButton("Options");
-		btnOptions.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				options.setVisible(true);
-			}
-		});
-
-		// Quit Game button
-		btnQuitGame = new JButton("Quit");
-		btnQuitGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String msg = "Are you sure you want to quit?";
-				int res = JOptionPane.showConfirmDialog(rootPane, msg);
-
-				if (res == JOptionPane.YES_OPTION)
-					System.exit(0);
-			}
-		});
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
 	}
 	
-	
-	private void addButtons() {
-		buttonsPanel.setLayout(new GridLayout(1, 3));
-		buttonsPanel.add(btnNewGame);
-
-
-		buttonsPanel2.setLayout(new GridLayout(1, 2));
-		buttonsPanel2.add(btnOptions);
-		buttonsPanel2.add(btnQuitGame);
-
-		getContentPane().add(buttonsPanel, BorderLayout.NORTH);
-		getContentPane().add(buttonsPanel2, BorderLayout.SOUTH);
+	public int getMazeSize(){
+		int x = mazeDimensions.getValue();
+		if ((x % 2) == 0) {
+			x++;
+		}
+		return x;
 	}
+
 	
-	public void init() {
-		setSize(534, 401);
 
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+	
 
-		setVisible(true);
-	}
+	
+
+
 }

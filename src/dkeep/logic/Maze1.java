36a8bf3a0028835e0207fd34d5e3040484a0ddc7
davidrsfalcao <davidrsfalcao@gmail.com>
2017-03-22 12:27:12 +0000

    Simplifiquei construtor maze1

--- src/dkeep/logic/Maze1.java
+++ src/dkeep/logic/Maze1.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 
 public class Maze1 extends Map{
 
+	char[][]temp={{'X','X','X','X','X','X','X','X','X','X'} ,
+			  {'X',' ',' ',' ','I',' ','X',' ',' ','X'} ,
+			  {'X','X','X',' ','X','X','X',' ',' ','X'} ,
+			  {'X',' ','I',' ','I',' ','X',' ',' ','X'} ,
+			  {'X','X','X',' ','X','X','X',' ',' ','X'} ,
+			  {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'} ,
+			  {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'} ,
+			  {'X','X','X',' ','X','X','X','X',' ','X'} ,
+			  {'X',' ','I',' ','I',' ','X','k',' ','X'} ,
+			  {'X','X','X','X','X','X','X','X','X','X'} };
+	
 	/**
 	 * Constructor of 1st Maze
 	 * 
@@ -11,50 +22,12 @@ public class Maze1 extends Map{
 	public Maze1(){
 		MAP_SIZE = 10; 
 		map = new char[MAP_SIZE][MAP_SIZE];
-		ArrayList<Integer> hero = new ArrayList<Integer>();
-		hero.add(1); //posX
-		hero.add(1); //posY
-		hero.add(0); //has_key
-		hero.add(0); //is_armed
-		
-		ArrayList<Integer> guard = new ArrayList<Integer>();
-		guard.add(8); //posX
-		guard.add(1); //posY
-		guard.add(1); //playing
-		
-		ArrayList<Integer> ogre = new ArrayList<Integer>();
-		ogre.add(0); //playing
-		
-		initValues.add(hero);
-		initValues.add(guard);
-		initValues.add(ogre);
-		
-		key = new Position(7,8,'k');
-		key.setType(1);
-		
-		
-		char[][]temp={{'X','X','X','X','X','X','X','X','X','X'} ,
-					  {'X',' ',' ',' ','I',' ','X',' ',' ','X'} ,
-					  {'X','X','X',' ','X','X','X',' ',' ','X'} ,
-					  {'X',' ','I',' ','I',' ','X',' ',' ','X'} ,
-					  {'X','X','X',' ','X','X','X',' ',' ','X'} ,
-					  {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'} ,
-					  {'I',' ',' ',' ',' ',' ',' ',' ',' ','X'} ,
-					  {'X','X','X',' ','X','X','X','X',' ','X'} ,
-					  {'X',' ','I',' ','I',' ','X','k',' ','X'} ,
-					  {'X','X','X','X','X','X','X','X','X','X'} };
-		int i = 0;
-		for( char[] c : temp){
-			map[i] = (char[])c.clone();
-			i++;
-		}
-		
-		Position t1 = new Position(0,5,'S');
-		endPositions = new ArrayList<Position>();
-		endPositions.add(t1);
-		Position t2 = new Position(0,6,'S');
-		endPositions.add(t2);
-		
+		setHero();
+		setGuard();
+		setOgres();
+		setObjectives();
+		setMap();
+	
 	}
 	
 	/**
@@ -74,9 +47,48 @@ public class Maze1 extends Map{
 		
 	};
 	
-	public void setObjectives()
+	protected void setObjectives()
 	{
+		key = new Position(7,8,'k');
+		key.setType(1);
+		Position t1 = new Position(0,5,'S');
+		endPositions = new ArrayList<Position>();
+		endPositions.add(t1);
+		Position t2 = new Position(0,6,'S');
+		endPositions.add(t2);
+	}
+	
+	protected void setHero()
+	{
+		ArrayList<Integer> hero = new ArrayList<Integer>();
+		hero.add(1); //posX
+		hero.add(1); //posY
+		hero.add(0); //has_key
+		hero.add(0); //is_armed
+		initValues.add(hero);
+	}
+	
+	protected void setGuard(){
+		ArrayList<Integer> guard = new ArrayList<Integer>();
+		guard.add(8); //posX
+		guard.add(1); //posY
+		guard.add(1); //playing
+		initValues.add(guard);
 		
+	}
+	
+	protected void setOgres(){
+		ArrayList<Integer> ogre = new ArrayList<Integer>();
+		ogre.add(0); //playing
+		initValues.add(ogre);
+	}
+	
+	protected void setMap() {
+		int i = 0;
+		for (char[] c : temp) {
+			map[i] = (char[]) c.clone();
+			i++;
+		}
 	}
 }
 	

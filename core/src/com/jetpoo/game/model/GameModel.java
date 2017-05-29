b518f324f039dce578f8f9c975bf8a0ca7e07afb
davidrsfalcao <davidrsfalcao@gmail.com>
2017-05-29 11:41:21 +0100

    update

--- core/src/com/jetpoo/game/model/GameModel.java
+++ core/src/com/jetpoo/game/model/GameModel.java
@@ -1,14 +1,34 @@
 package com.jetpoo.game.model;
 
-/**
- * Created by davidfalcao on 23/04/17.
- */
+import com.jetpoo.game.model.entities.CharacterModel;
 
 /**
+ * Created by davidfalcao on 23/04/17.
+ *
  * A model representing a game.
  */
 public class GameModel {
 
+    /**
+     * The singleton instance of the game model
+     */
+    private static GameModel instance;
+
+    /**
+     * The character controlled by the user in this game.
+     */
+    private CharacterModel character;
+
+    /*
+     * Returns a singleton instance of the game model
+     *
+     * @return the singleton instance
+     */
+    public static GameModel getInstance() {
+        if (instance == null)
+            instance = new GameModel();
+        return instance;
+    }
 
 
 }

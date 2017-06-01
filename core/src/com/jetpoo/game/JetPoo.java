a847079cd26b85c1f1c42a24c4f8dd71c839e2fe
davidrsfalcao <davidrsfalcao@gmail.com>
2017-06-01 15:00:39 +0100

    update

--- core/src/com/jetpoo/game/JetPoo.java
+++ core/src/com/jetpoo/game/JetPoo.java
@@ -1,61 +1,76 @@
 package com.jetpoo.game;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.jetpoo.game.view.GameView;
+import com.jetpoo.game.states.GameStateManager;
+import com.jetpoo.game.states.MenuState;
 
-public class JetPoo extends Game {
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Text;
+
+public class JetPoo extends ApplicationAdapter {
+
+	private static JetPoo instance;
+
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+
+	public static final String TITLE = "JetPoo";
+	private GameStateManager gsm;
 	private SpriteBatch batch;
 	private AssetManager assetManager;
 
-	/**
-	 * Creates the game. Initializes the sprite batch and asset manager.
-	 * Also starts the game until we have a main menu.
-	 */
+	private Music music;
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 		assetManager = new AssetManager();
-
+		batch = new SpriteBatch();
+		gsm = new GameStateManager(this);
 		startGame();
+
 	}
 
 	/**
 	 * Starts the game.
 	 */
 	private void startGame() {
-
-		setScreen(new GameView(this));
+		assetManager.load("birdanimation.png", Texture.class);
+		assetManager.finishLoading();
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 
-	/**
-	 * Disposes of all assets.
-	 */
 	@Override
-	public void dispose () {
-		batch.dispose();
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 		assetManager.dispose();
 	}
 
 
-	/**
-	 * Returns the asset manager used to load all textures and sounds.
-	 *
-	 * @return the asset manager
-	 */
-	public AssetManager getAssetManager() {
+	public AssetManager getAssetManager(){
 		return assetManager;
 	}
 
-
-	/**
-	 * Returns the sprite batch used to improve drawing performance.
-	 *
-	 * @return the sprite batch
-	 */
-	public SpriteBatch getBatch() {
-		return batch;
+	public static JetPoo getInstance(){
+		return instance;
 	}
 }

aff7ba27a270d88fd4f65deaaf189b34316e7ee2
davidrsfalcao <davidrsfalcao@gmail.com>
2017-06-03 16:22:36 +0100

    update

--- core/src/com/jetpoo/game/states/MenuState.java
+++ core/src/com/jetpoo/game/states/MenuState.java
@@ -1,6 +1,7 @@
 package com.jetpoo.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -11,17 +12,35 @@ public class MenuState extends State{
     private Texture background;
     private Sprite playBtn;
     private Texture loginBtn;
-    private Texture leaderboardBtn;
+    private Sprite leaderboardBtn;
+    private Sprite creditsBtn;
+
     private GameStateManager gsm;
+    private float screenWidth_con;
+    private float screenHeight_con;
+    private int resX = 1024;
+    private int resY = 768;
+
     public MenuState(final GameStateManager gsm) {
         super(gsm);
         this.gsm = gsm;
-        cam.setToOrtho(false, 1024, 768);
-        background = new Texture("bg.png");
-        playBtn = new Sprite(new Texture("playbtn.png"));
-        //playBtn.setBounds(170,190, 185, 38);
-        playBtn.setBounds(0,0, 200, 200);
-        leaderboardBtn=new Texture("leaderboard.png");
+        this.screenWidth_con = (float) resX / (float) Gdx.graphics.getWidth();
+        this.screenHeight_con =  (float) resY/ (float) Gdx.graphics.getHeight();
+
+        cam.setToOrtho(false, resX, resY);
+
+        background = new Texture("Menu_bg.png");
+
+        playBtn = new Sprite(new Texture("button_play.png"));
+        playBtn.setBounds(7/screenWidth_con , 7/screenHeight_con, 297/screenWidth_con , 100/screenHeight_con);
+
+        leaderboardBtn= new Sprite(new Texture("button_leaderboard.png"));
+        leaderboardBtn.setBounds(7/screenWidth_con , 127/screenHeight_con  , 297/screenWidth_con, 100/screenHeight_con );
+
+
+        creditsBtn = new Sprite(new Texture("button_credits.png"));
+        creditsBtn.setBounds(7/screenWidth_con , 247/screenHeight_con  , 297/screenWidth_con, 100 /screenHeight_con);
+
         loginBtn=new Texture("login.png");
 
         Gdx.input.setInputProcessor(new InputAdapter(){
@@ -29,10 +48,21 @@ public class MenuState extends State{
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 
+
                 if(playBtn.getBoundingRectangle().contains(screenX, screenY)){
                     gsm.set(new PlayState(gsm));
-                    System.out.println("Play button");
                 }
+
+
+                if(leaderboardBtn.getBoundingRectangle().contains(screenX, screenY)){
+                    gsm.set(new LeaderBoardState(gsm));
+                }
+
+                if(creditsBtn.getBoundingRectangle().contains(screenX, screenY)){
+                    //gsm.set(new LeaderBoardState(gsm));
+                    System.out.println("Creditos");
+                }
+
 
                 return true;
             }
@@ -58,17 +88,19 @@ public class MenuState extends State{
 
     @Override
     public void update(float dt) {
-        //handleInput();
+        //Do nothing
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(background, 0,0);
-        sb.draw(playBtn, 270, 400);
-        sb.draw(leaderboardBtn,cam.position.x - leaderboardBtn.getWidth()/2,cam.position.y-50);
-        sb.draw(loginBtn,10,cam.position.y-200);
+        sb.draw(background, 0,0, resX, resY);
+        sb.draw(playBtn, 7 , resY-(107), 297  , 100);
+        sb.draw(leaderboardBtn, 7 , resY-(227), 297  , 100);
+        sb.draw(creditsBtn, 7 , resY-(347), 297  , 100);
+
+        //sb.draw(loginBtn,10,cam.position.y-200);
         sb.end();
     }
 
@@ -76,7 +108,7 @@ public class MenuState extends State{
     public void dispose() {
         background.dispose();
         //playBtn.dispose();
-        leaderboardBtn.dispose();
+        //leaderboardBtn.dispose();
         loginBtn.dispose();
         Gdx.input.setInputProcessor(null);
     }

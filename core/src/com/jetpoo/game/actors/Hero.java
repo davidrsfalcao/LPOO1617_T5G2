d37ce3bac0377a5d419451c512985aac2d05c067
davidrsfalcao <davidrsfalcao@gmail.com>
2017-06-05 21:56:27 +0100

    update

--- core/src/com/jetpoo/game/actors/Hero.java
+++ core/src/com/jetpoo/game/actors/Hero.java
@@ -46,7 +46,7 @@ public abstract class Hero {
     }
 
     public abstract void jump();
-    
+
     public boolean isAcelerating() {
         return acelerating;
     }
--- desktop/src/com/jetpoo/game/desktop/DesktopLauncher.java
+++ desktop/src/com/jetpoo/game/desktop/DesktopLauncher.java
@@ -3,10 +3,12 @@ package com.jetpoo.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jetpoo.game.JetPoo;
+import com.jetpoo.game.Server.PlayServices;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new JetPoo(), config);
+		PlayServices playServices;
+		new LwjglApplication(new JetPoo(playServices), config);
 	}
 }
